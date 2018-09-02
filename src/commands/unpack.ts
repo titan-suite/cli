cd57d5434cd8527e05034662f0696f636b7484c3
Tomisin <tomisinjenrola@gmail.com>
2018-09-02 19:19:45 -0400

    Added React Native pack

--- src/commands/unpack.ts
+++ src/commands/unpack.ts
@@ -1,39 +1,37 @@
-import { Command, flags } from '@oclif/command'
-import { prompt, Answers } from 'inquirer'
-import { downloadPack } from '../utils/index'
+import {Command, flags} from '@oclif/command'
+import {Answers, prompt} from 'inquirer'
+
+import {downloadPack} from '../utils/index'
 
 export default class Unpack extends Command {
     static description = 'Start a new project with a pre-built dApp'
 
     static examples = [
-        `$ titan unpack`,
-        `$ titan unpack <name>`,
+        '$ titan unpack',
+        '$ titan unpack <name>',
     ]
 
     static flags = {
-        help: flags.help({ char: 'h' }),
+        help: flags.help({char: 'h'}),
     }
 
-    static args = [{ name: 'pack' }]
-
+    static args = [{name: 'pack'}]
 
     async run() {
-        const { args } = this.parse(Unpack)
+        const {args} = this.parse(Unpack)
 
         if (args.pack) {
             downloadPack(args.pack, `titan-${args.pack}`)
-        }
-        else {
+        } else {
             let promptQuestions = [{
                 type: 'list',
                 name: 'selected_pack',
                 message: 'Choose a pack to download',
-                choices: ['react', 'default'],
+                choices: ['react', 'react-native', 'default'],
             }]
 
             const answer: Answers = await prompt(promptQuestions)
-            downloadPack(answer["selected_pack"], `titan-${answer["selected_pack"]}`)
-
+            downloadPack(answer.selected_pack, `titan-${answer.selected_pack}`)
         }
 
     }
