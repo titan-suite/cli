06f3dfa479a01c69fef8030247ac64be18ba6df2
Tomisin <tomisinjenrola@gmail.com>
2018-09-02 19:21:50 -0400

    Fixed bolt json creation

--- src/commands/deploy.ts
+++ src/commands/deploy.ts
@@ -1,28 +1,29 @@
-import { Command, flags } from '@oclif/command'
-import * as fs from 'fs'
-import * as notifier from 'node-notifier'
-import * as mkdirp from 'mkdirp'
-import * as path from 'path'
-import { prompt, Answers } from 'inquirer'
+import {Command, flags} from '@oclif/command'
 import cli from 'cli-ux'
-import { compile, deploy, readContract } from '../utils/index'
+import * as fs from 'fs'
+import {Answers, prompt} from 'inquirer'
+import * as mkdirp from 'mkdirp'
+import * as notifier from 'node-notifier'
+import * as path from 'path'
+
+import {compile, deploy, readContract} from '../utils/index'
 
 export default class Deploy extends Command {
     static description = 'Deploys a Solidity smart contract to an AION node'
 
     static examples = [
-        `$ titan deploy <path/to/Example.sol>`,
-        `$ titan deploy -n SpecificContract <path/to/ManyContracts.sol>`,
-        `$ titan deploy -p 5 <path/to/ContractWithParams.sol>`,
+        '$ titan deploy <path/to/Example.sol>',
+        '$ titan deploy -n SpecificContract <path/to/ManyContracts.sol>',
+        '$ titan deploy -p 5 <path/to/ContractWithParams.sol>',
     ]
 
     static flags = {
-        help: flags.help({ char: 'h' }),
-        name: flags.string({ char: 'n', description: 'specify which smart contract to deploy within the file' }),
-        params: flags.string({ char: 'p', description: 'pass parameters to the smart contract' }),
+        help: flags.help({char: 'h'}),
+        name: flags.string({char: 'n', description: 'specify which smart contract to deploy within the file'}),
+        params: flags.string({char: 'p', description: 'pass parameters to the smart contract'}),
     }
 
-    static args = [{ name: 'file' }]
+    static args = [{name: 'file'}]
 
     questions: Array<any> = [{
         type: 'list',
@@ -32,8 +33,11 @@ export default class Deploy extends Command {
     }]
 
     generateChoices(arr: string[]) {
-        for (const i in arr) {
-            this.questions[0].choices.push(arr[i])
+        this.log('generating choices for', arr)
+        // let i: any
+        for (let i of arr) {
+            this.log(i)
+            this.questions[0].choices.push(i)
         }
     }
 
@@ -46,43 +50,35 @@ export default class Deploy extends Command {
             await deploy(_abi, _code, _params) :
             await deploy(_abi, _code)
 
-        this.log("Successfully deployed!")
+        this.log('Successfully deployed!')
         this.log('Deployment Details:')
-        this.log("contract:", _name)
-        this.log("address:", deployedContract["contractAddress"])
-        this.log("transaction hash:", deployedContract["transactionHash"])
-        this.log("NRG used:", deployedContract["nrgUsed"])
-        this.log("block number:", deployedContract["blockNumber"])
-        this.log("from:", deployedContract["from"])
-        this.log("to:", deployedContract["to"])
-        this.log("logs:", deployedContract["logs"])
-
-        mkdirp("build/bolts", (err, made) => { if (err) throw err })
-
+        this.log('contract:', _name)
+        this.log('address:', deployedContract.contractAddress)
+        this.log('transaction hash:', deployedContract.transactionHash)
+        this.log('NRG used:', deployedContract.nrgUsed)
+        this.log('block number:', deployedContract.blockNumber)
+        this.log('from:', deployedContract.from)
+        this.log('to:', deployedContract.to)
+        this.log('logs:', deployedContract.logs)
 
         const boltsPath = path.join(process.cwd(), 'build', 'bolts', `${_name}.json`)
-        fs.open(boltsPath, 'w', (err, fd) => {
-            if (err) {
-                if (err.code === 'EEXIST') {
-                    fs.unlink(boltsPath, (err) => {
-                        if (err) throw err;
-                    })
-                }
-            }
 
+        mkdirp('build/bolts', err => {
+            if (err) throw err
             else {
                 const deployedContractDetails = {
-                    'contract': _name,
-                    'abi': _abi,
-                    'deployed_address': deployedContract["contractAddress"],
-                    'transaction_hash': deployedContract["transactionHash"],
-                    'block_number': deployedContract["blockNumber"]
+                    contract: _name,
+                    abi: _abi,
+                    deployed_address: deployedContract.contractAddress,
+                    transaction_hash: deployedContract.transactionHash,
+                    block_number: deployedContract.blockNumber
                 }
-                fs.writeFile(boltsPath, JSON.stringify(deployedContractDetails, null, 4), (err) => {
+                fs.writeFile(boltsPath, JSON.stringify(deployedContractDetails, null, 4), err => {
                     if (err) throw err
                 })
             }
-        });
+        })
+
         cli.action.stop()
 
         notifier.notify({
@@ -92,7 +88,7 @@ export default class Deploy extends Command {
     }
 
     async run() {
-        const { args, flags } = this.parse(Deploy)
+        const {args, flags} = this.parse(Deploy)
 
         const sol = readContract(args.file)
         const compiledContract: any = await compile(sol)
@@ -101,16 +97,17 @@ export default class Deploy extends Command {
 
         if (flags.name) {
             contractName = flags.name
-            this.handleDeploy(contractName, compiledContract, flags.params)
+            await this.handleDeploy(contractName, compiledContract, flags.params)
         } else if (Object.keys(compiledContract).length === 1) {
             contractName = Object.keys(compiledContract)[0]
-            this.handleDeploy(contractName, compiledContract, flags.params)
+            await this.handleDeploy(contractName, compiledContract, flags.params)
         } else {
+            Object(compiledContract).entries((i: any) => this.log(i))
             this.generateChoices(Object.keys(compiledContract))
 
             const answer: Answers = await prompt(this.questions)
-            contractName = answer["selected_contract"]
-            this.handleDeploy(contractName, compiledContract, flags.params)
+            contractName = answer.selected_contract
+            await this.handleDeploy(contractName, compiledContract, flags.params)
         }
     }
 }
